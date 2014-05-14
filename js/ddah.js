1ee8e0776cdb162c275a7675a90e2e530b2ae052
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-05-14 16:04:51 -0400

    add modal and add a specific file for d3-config-graph

--- js/ddah.js
+++ js/ddah.js
@@ -10,10 +10,12 @@ function showInfo(data, tabletop) {
         $("#data-content").append("<div class='row fila"+fila+"'>");
       
       var promesa   = sheet.elements[i].promesa;
-      var detalles  = sheet.elements[i].detalles;
+      var detail    = sheet.elements[i].detalles;
+      var min_detail= sheet.elements[i].detalles.substr(0,215);
       var tipo      = '';
       var tmp_tipo  = sheet.elements[i].tipo;
       var area      = sheet.elements[i].area;
+      var view_more = ''
 
       var cnt_tipo = tmp_tipo.split(',');
 
@@ -28,10 +30,14 @@ function showInfo(data, tabletop) {
       {
         tipo = '<li class="type-'+tmp_tipo.toLowerCase()+'">'+tmp_tipo+'</li>';
       }
+
+      if(detail.length > 215)
+        view_more = '...<a href="#" data-toggle="modal" data-target="#myModal-'+i+'">ver más</a>'
       
       var twitter = '<a href="#" onclick="window.open(\'https://twitter.com/share?url=http://deldichoalhecho.cl/&amp;via=ciudadanoi&amp;hashtags=deldichoalhecho&amp;text='+promesa+'\',\'twitter\',\'width=450, height=250\')">twitter</a>';
+      var modal = '<div class="modal fade" id="myModal-'+i+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title" id="myModalLabel">'+promesa+'</h4></div><div class="modal-body">'+detail+'</div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button></div></div></div></div>';
 
-      $(".fila"+fila).append("<div class='col-md-3 cajita'><span class='area'>"+area+"</span><ul id='social'><li>"+twitter+"</li></ul><h2 id='title'>"+promesa+"</h2><p class='detail'>"+detalles+"</p><ul id='type'>"+tipo+"</ul></div>");
+      $(".fila"+fila).append("<div class='col-md-3 cajita'><span class='area'>"+area+"</span><ul id='social'><li>"+twitter+"</li></ul><h2 id='title'>"+promesa+"</h2><p class='detail'>"+min_detail+view_more+"</p><ul id='type'>"+tipo+"</ul></div>"+modal);
 
       cnt++;
       
