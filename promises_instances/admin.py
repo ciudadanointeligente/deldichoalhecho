fde89facaa169728484512fe045d87fed0de7558
Felipe Álvarez <falvarez@votainteligente.cl>
2015-03-02 13:20:43 -0300

    Using DDAH categories in admin instead of normal categories

--- promises_instances/admin.py
+++ promises_instances/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from promises.models import Category
+from promises_instances.models import DDAHCategory
 
 # Register your models here.
+
+admin.site.unregister(Category)
+
+
+class DDAHCategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(DDAHCategory, DDAHCategoryAdmin)
