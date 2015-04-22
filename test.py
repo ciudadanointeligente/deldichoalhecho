43e0fe314ebf509d8754ca25a463e22abd22c30b
Felipe Álvarez <falvarez@votainteligente.cl>
2015-04-22 16:13:20 -0300

    Trying to fix travis

--- test.py
+++ test.py
@@ -3,4 +3,3 @@ from django.core.management import call_command
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_site.settings")
 call_command('test', 'promises_web', 'promises_instances', verbosity=1)
-
