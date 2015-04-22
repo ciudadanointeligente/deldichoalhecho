b41bad66f56a38ddf06b956317a5dbe99d1b120d
Felipe Álvarez <falvarez@votainteligente.cl>
2015-04-22 16:58:47 -0300

    Updating migration to point to first

--- promises_instances/migrations/0002_ddahinstance.py
+++ promises_instances/migrations/0002_ddahinstance.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('instances', '0002_auto_20150422_1939'),
+        ('instances', '__first__'),
         ('promises_instances', '0001_initial'),
     ]
 
