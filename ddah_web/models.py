c80cba910b0f356dcf3a56e097fc8fceeb102aff
Felipe Álvarez <falvarez@votainteligente.cl>
2015-04-23 17:58:08 -0300

    Getting the instance as json

--- ddah_web/models.py
+++ ddah_web/models.py
@@ -1,8 +1,19 @@
 from promises_instances.models import DDAHInstance
 from django.db import models
 from ddah_web import read_template_as_string
+from bunch import Bunch
+from promises.models import Promise
+import json
+import datetime
 
 
+def default_json_encoder(o):
+    if isinstance(0, datetime.date) or isinstance(0, datetime.datetime):
+        return o.isoformat()
+
+
+# This could easily be a proxy model
+# I'm waiting until the end of the day to see what happens
 class DDAHInstanceWeb(DDAHInstance):
     def save(self, *args, **kwargs):
         creating = self.id is None
@@ -10,6 +21,69 @@ class DDAHInstanceWeb(DDAHInstance):
         if creating:
             DDAHTemplate.objects.create(instance=self)
 
+    def get_as_bunch(self):
+        me = Bunch(label=self.label, title=self.title)
+        categories = Bunch()
+        for category in self.categories.all():
+            cat_bunch = Bunch(id=category.id, name=category.name, slug=category.slug)
+            categories[category.id] = cat_bunch
+            promises = Bunch()
+            for promise in category.promises.all():
+                promise_bunch = Bunch(id=promise.id,
+                    name=promise.name,
+                    description=promise.description,
+                    date=promise.date)
+                promise_bunch.fulfillment = Bunch(percentage=promise.fulfillment.percentage,
+                                                  status=promise.fulfillment.status,
+                                                  description=promise.fulfillment.description
+                                                  )
+                verification_documents = Bunch()
+                for verification_document in promise.verification_documents.all():
+                    v_d_bunch = Bunch(id=verification_document.id,
+                                      url=verification_document.url,
+                                      display_name=verification_document.display_name,
+                                      )
+                    verification_documents[verification_document.id] = v_d_bunch
+                promise_bunch.verification_documents = verification_documents
+                information_sources = Bunch()
+                for information_source in promise.information_sources.all():
+                    i_s_bunch = Bunch(id=information_source.id,
+                                      url=information_source.url,
+                                      display_name=information_source.display_name,
+                                      )
+                    information_sources[information_source.id] = i_s_bunch
+                promise_bunch.information_sources = information_sources
+
+                milestones = Bunch()
+                for milestone in promise.milestones.all():
+                    milestones_bunch = Bunch(id=milestone.id,
+                                             date=milestone.date,
+                                             description=milestone.description,
+                                             )
+                    milestones[milestone.id] = milestones_bunch
+                promise_bunch.milestones = milestones
+
+                promises[promise.id] = promise_bunch
+
+            cat_bunch.promises = promises
+
+        me.categories = categories
+        summary = Promise.objects.filter(category__in=self.categories.all()).summary()
+        me.summary = Bunch(
+            no_progress=summary.no_progress,
+            accomplished=summary.accomplished,
+            in_progress=summary.in_progress,
+            total=summary.total,
+            total_progress=summary.total_progress,
+            accomplished_percentage=summary.accomplished_percentage,
+            in_progress_percentage=summary.in_progress_percentage,
+            no_progress_percentage=summary.no_progress_percentage,
+            )
+        return me
+
+    def to_json(self):
+        bunchified = self.get_as_bunch()
+        return json.dumps(bunchified, default=default_json_encoder)
 
 default_template = read_template_as_string('instance_templates/default.html')
 
--- ddah_web/tests/ddah_instances_web_tests.py
+++ ddah_web/tests/ddah_instances_web_tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from ddah_web.models import DDAHInstanceWeb
 from promises_instances.models import DDAHInstance
+from promises.models import Promise
 
 
 class DDAHInstanceWebTestCase(TestCase):
+    fixtures = ['100dias.json']
+
     def setUp(self):
         pass
 
@@ -12,3 +15,55 @@ class DDAHInstanceWebTestCase(TestCase):
 
         instance = DDAHInstanceWeb.objects.create(label="bici", title="Bicicletas")
         self.assertIsInstance(instance, DDAHInstance)
+
+    def test_instance_to_bunch(self):
+        instance = DDAHInstanceWeb.objects.get(id=1)
+        the_bunch = instance.get_as_bunch()
+        self.assertEquals(the_bunch.title, instance.title)
+        self.assertEquals(len(the_bunch.categories), instance.categories.count())
+
+        for category_id in the_bunch.categories:
+            category = the_bunch.categories[category_id]
+            the_cat_from_database = instance.categories.get(id=category.id)
+            self.assertEquals(the_cat_from_database.name, category.name)
+            self.assertEquals(the_cat_from_database.slug, category.slug)
+            self.assertEquals(the_cat_from_database.promises.count(), len(category.promises))
+            for promise_id in category.promises:
+                promise = category.promises[promise_id]
+                the_promise_from_database = the_cat_from_database.promises.get(id=promise.id)
+                self.assertEquals(the_promise_from_database.name, promise.name)
+                self.assertEquals(the_promise_from_database.description, promise.description)
+                self.assertTrue(promise.date)
+                self.assertEquals(the_promise_from_database.fulfillment.percentage, promise.fulfillment.percentage)
+                self.assertEquals(the_promise_from_database.fulfillment.status, promise.fulfillment.status)
+                self.assertEquals(the_promise_from_database.fulfillment.description, promise.fulfillment.description)
+                for verification_doc_id in promise.verification_documents:
+                    verification_doc = promise.verification_documents[verification_doc_id]
+                    the_verification_doc_from_database = the_promise_from_database.verification_documents.get(id=verification_doc.id)
+                    self.assertEquals(the_verification_doc_from_database.url, verification_doc.url)
+                    self.assertEquals(the_verification_doc_from_database.display_name, verification_doc.display_name)
+
+                for information_source_id in promise.information_sources:
+                    information_source = promise.information_sources[information_source_id]
+                    the_information_source_from_database = the_promise_from_database.information_sources.get(id=information_source.id)
+                    self.assertEquals(the_information_source_from_database.url, information_source.url)
+                    self.assertEquals(the_information_source_from_database.display_name, information_source.display_name)
+                for milestone_id in promise.milestones:
+                    milestone = promise.milestones[milestone_id]
+                    milestone_from_db = the_promise_from_database.milestones.get(id=milestone.id)
+                    self.assertEquals(milestone_from_db.description, milestone.description)
+                    self.assertTrue(milestone.date)
+
+        expected_summary = Promise.objects.filter(category__in=instance.categories.all()).summary()
+        self.assertEquals(the_bunch.summary.no_progress, expected_summary.no_progress)
+        self.assertEquals(the_bunch.summary.accomplished, expected_summary.accomplished)
+        self.assertEquals(the_bunch.summary.in_progress, expected_summary.in_progress)
+        self.assertEquals(the_bunch.summary.total, expected_summary.total)
+        self.assertEquals(the_bunch.summary.total_progress, expected_summary.total_progress)
+        self.assertEquals(the_bunch.summary.accomplished_percentage, expected_summary.accomplished_percentage)
+        self.assertEquals(the_bunch.summary.in_progress_percentage, expected_summary.in_progress_percentage)
+        self.assertEquals(the_bunch.summary.no_progress_percentage, expected_summary.no_progress_percentage)
+
+    def test_as_json(self):
+        instance = DDAHInstanceWeb.objects.get(id=1)
+        self.assertTrue(instance.to_json())
